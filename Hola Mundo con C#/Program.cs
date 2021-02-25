ba4d86f587896ea643e81daa81b923121254204c
Kevin Borge Tobar <kevinborgetobar1998@gmail.com>
2021-02-25 00:54:33 -0600

    Hola Mundo con C#

---
+++ Hola Mundo con C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Proyecto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo desde C#");
+            Console.WriteLine("Adios");
+            Console.WriteLine("Fin del Programa");
+        }
+    }
+}
